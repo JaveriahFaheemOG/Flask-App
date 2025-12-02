102189cbac00ff626e23c7e0a5677842a7adb10e
Javeriah Faheem <javeriahfaheem16@gmail.com>
2025-12-02 15:17:27 +0500

    added 2 by javeriah faheem 22i7421

--- app.py
+++ app.py
@@ -63,5 +63,7 @@ def delete_user(sno):
 
 if __name__ == '__main__':
     with app.app_context():
+        print("Making changes through the other cloned directory..")
         db.create_all()
     app.run(debug=True)
+  
